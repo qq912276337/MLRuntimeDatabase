19dd1dcd8f98095725ae657da61b7c137cd2555c
sml <912276337@qq.com>
2016-07-26 10:21:58 +0800

    完成主键优化，查询不需要传主键

--- BaseProject/BaseProject/ContentViewController/ContentViewController.m
+++ BaseProject/BaseProject/ContentViewController/ContentViewController.m
@@ -20,36 +20,59 @@
 
 - (void)viewDidLoad{
     [super viewDidLoad];
-    [self addLeftNavigationItemWithImage:@"箭头二" size:CGSizeMake(30, 30) target:self action:@selector(clickContentVcLeftItem)];
-    NSInteger i = 1;
-//    for (NSInteger i =0; i < 5; i++) {
+    //[self addLeftNavigationItemWithImage:@"箭头二" size:CGSizeMake(30, 30) target:self action:@selector(clickContentVcLeftItem)];
+//    NSInteger i = 1;
+    for (NSInteger i =2; i < 5; i++) {
+        /** 模型对象1 */
         BSTeacher *tea = [BSTeacher new];
         tea.id = i;
         tea.name = [NSString stringWithFormat:@"%@%ld",NSStringFromClass([tea class]),i];
         
+        /** 模型对象2 */
         BSStudent *stu = [BSStudent new];
         stu.id = i;
         stu.name = [NSString stringWithFormat:@"%@%ld",NSStringFromClass([stu class]),i];
         stu.tea = tea;
         
+        /** 模型对象0 */
         BSUser *user = [BSUser new];
         user.id = i;
         user.name = [NSString stringWithFormat:@"%@%ld",NSStringFromClass([user class]),i];
         user.stu = stu;
         user.data = [@"12323435" dataUsingEncoding:NSUTF8StringEncoding];
-        MMLog(@"------%@-",user.data);
         user.arr = @[tea,tea,tea];
     
     [[MLDatabaseQueue dbQueue] inDatabase:^(FMDatabase *db) {
-            [db ml_saveDataWithModel:user primaryKey:@"id" option:^(BOOL save) {
-                
-            }];
+//            /** 存储:会自动调用insert或者update，不需要担心重复插入数据 */
+//            [db ml_saveDataWithModel:user primaryKey:@"id" option:^(BOOL save) {
+//            }];
+//        
+        
+//        /** 查询某个 */
+//        BSUser *user0 = [BSUser new];
+//        user0.id = user.id;
+//        [db ml_excuteDataWithModel:user0 primaryKey:@"id" option:^(BSUser *output_model) {
+//            MMLog(@"-----%@--",output_model.data);
+//        }];
+        
+        /** 查询某种的所有数据 */
         BSUser *user0 = [BSUser new];
-        user0.id = user.id;
-        [db ml_excuteDataWithModel:user0 primaryKey:@"id" option:^(BSUser *output_model) {
-            MMLog(@"-----%@--",output_model.data);
+        [db ml_excuteDatasWithModel:user0  option:^(NSMutableArray *models) {
+            
         }];
+        
+//        /** 删除 */
+//        BSUser *user0 = [BSUser new];
+//        user0.id = user.id;
+//        [db ml_deleteDataWithModel:user0 primaryKey:@"id" option:^(BOOL del) {
+//            MMLog(@"------%ld-",del);
+//        }];
     }];
+    
+    }
+    
+    
+    
     
     
 }
